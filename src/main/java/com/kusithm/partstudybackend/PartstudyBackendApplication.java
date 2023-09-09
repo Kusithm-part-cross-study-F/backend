aa846cded56aef218376f110e1822b4925f338d7
OJOJIN <ojy101039@gmail.com>
2023-09-09 13:34:58 +0900

    init

---
+++ src/main/java/com/kusithm/partstudybackend/PartstudyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kusithm.partstudybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartstudyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartstudyBackendApplication.class, args);
+    }
+
+}
